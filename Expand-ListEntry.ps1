87ba8e4af04f33c620218b5ca13048f79440af22
Eric K. Miller <ekmiller531@gmail.com>
2025-12-20 20:38:20 -0500

    Upload Expand-ListEntry.ps1

---
+++ Expand-ListEntry.ps1
@@ -0,0 +1,42 @@
+﻿<#
+Data Analysis: Expand-ListEntry.ps1
+Author: Eric K. Miller
+Last updated: 20 December 2025
+
+This script contains PowerShell code to "unravel" rows that contain
+lists as entries, creating a new row for each entry in a PSCustomObject.
+It enables use of crosstab, in particular.
+#>
+
+function Expand-ListEntry {
+    [CmdletBinding(SupportsShouldProcess)]
+    [Alias('explode')]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [PSCustomObject[]]$DataObject,
+
+        [Parameter(Mandatory)]
+        [string]$ExplodeField
+    )
+
+    process {
+        foreach ($item in $DataObject) {
+            $list = $item.$ExplodeField
+
+            if ($list -is [string]) {
+                $list = $list -split ',' | ForEach-Object {$_.Trim()}
+            }
+            elseif ($list -eq $null) {
+                $output = $item.PSObject.Copy()
+                $output.$ExplodeField = $null
+                $output
+                continue
+            }
+            foreach ($val in $list) {
+                $output = $item.PSObject.Copy()
+                $output.$ExplodeField = $val
+                $output
+            }
+        }
+    }
+}
